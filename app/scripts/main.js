5031bee2760582d7de4498de5d6e386ca3aef08e
Alex Hertz <alexhertz@designbyah.com>
2013-10-16 16:02:18 -0500

    Done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  var endDate = "October 22, 2013 10:00:00";
+
+  $('.countdown').countdown({
+    date: endDate,
+    render: function(data) {
+      $(this.el).html("<li>" + (data.days, 3) + " days</li><li>" + this.leadingZeros(data.hours, 2) + " hrs</li><li>" + this.leadingZeros(data.min, 2) + " min</li><li>" + this.leadingZeros(data.sec, 2) + " sec</li>");
+    }
+  });
+});
