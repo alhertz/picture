e64d80508744761010f7ae4e9d74755a161747bc
Alex Hertz <alexhertz@designbyah.com>
2013-10-20 15:03:53 -0500

    Gh-Pages Branch

---
+++ app/download/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Download She's Gonna Take A Picture Too - Sun Hotel</title>
+        <meta name="description" content="It's been over two years since we've released a new album and although we've been sharing bits and peices of new music to come, we haven't actually released anything. Naturally, that's been driving us crazy and we've decided to change that.">
+        <meta name="viewport" content="width=device-width">
+        <!-- Facebook Meta -->
+        <meta property="og:title" content="She's Gonna Take A Picture Too - Sun Hotel">
+        <meta property="og:image" content="http://picture.sunhotelsounds.com/images/artwork.jpg">
+        <meta property="og:site_name" content="She's Gonna Take A Picture Too">
+        <meta property="og:description" content="It's been over two years since we've released a new album and although we've been sharing bits and peices of new music to come, we haven't actually released anything. Naturally, that's been driving us crazy and we've decided to change that.">
+        <link rel="icon" type="image/png" href="favicon.png">
+        <link rel="stylesheet" href="../styles/23e5c62b.main.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!--[if lt IE 10]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <!-- Preloader -->
+        <div id="preloader">
+          <div id="status"><img src="../images/4239ba80.loading.gif"></div>
+        </div>
+        <!--Static Layer-->
+        <div class="static"></div>
+        <!--Content-->
+        <div class="hero">
+            <h1>Allo, allo!</h1>
+            <p>You've made it! You must be a very special person to have made it here. 
+               What a journey it must have been discovering this deep dark mystical corner of our web presence...</p>
+            <p>Anyways, if you're here, that probably means you have a download card in your posession with a code on it.
+                Go ahead and enter that code below and you're all set! If you have any issues, give us a shout at 
+                sunhotelsounds@gmail.com or hit us up on <a href="http://www.twitter.com/sunhotel">twitter</a>.</p>
+            <h2>Enter Your Code Below</h2>
+            <?php include_once('/home/desigom9/cashmusic-sunhotel/framework/cashmusic.php');CASHSystem::embedElement(1); // (Picture Single Download) ?>
+        </div>
+        <div class="cash">
+            <a href="http://cashmusic.org/" title="It's The Fucking Jam">Built Using Cash</a>
+        </div>
+        <script src="../scripts/1137e1f5.vendor.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-11588562-12', 'sunhotelsounds.com');
+          ga('send', 'pageview');
+        </script>
+        <!--Typekit-->
+        <script type="text/javascript" src="//use.typekit.net/hud6zhv.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+        <script src="../scripts/9088fe89.main.js"></script>
+</body>
+</html>
